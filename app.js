24f6e5d9eb6aa9184051bce13a1f06f4119f6632
Your Name <you@example.com>
2024-12-31 14:48:51 +0500

    Arrow functions (Basic)

---
+++ app.js
@@ -0,0 +1,31 @@
+// Arrow Functions:
+
+// 1. Basic Syntax
+
+// const cal = (a, b) => { console.log(a + b) }
+// cal (77, 96)
+
+// 2. No Parameters
+
+// const greet = _ => { console.log("Hello World"); }
+// greet ()
+
+// 3. Single Parameter
+
+// const square = x => x * x
+// console.log(square (5));
+
+// 4. Multiple Parameters
+
+// const value = (x, y, z) => x + y + z
+// console.log(value (10, 20, 30));
+
+// let val1 = 5;
+// let val2 = 8;
+// const addition = _ => val1+val2
+// console.error(addition());
+
+// let massage = "Hello World";
+// const user = _ => console.error(massage)
+// console.log(user());
+
